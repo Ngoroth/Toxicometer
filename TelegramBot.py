0e29f8f645e83babea382cc143d0ceeed3313525
Ngoroth <ngoroth@yandex.ru>
2020-03-29 18:08:36 +0300

    Add inlline search of users toxicity

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,13 +1,17 @@
 import logging
 
-from telegram import Update, User
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, PicklePersistence
+from telegram import Update, User, InlineQueryResultArticle, ParseMode, InputTextMessageContent
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, PicklePersistence, \
+    InlineQueryHandler
+from uuid import uuid4
 
 import SentimentAnalyzer
 from Models import UserToxicityData
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger(__name__)
 
 
 def start(update: Update, context: CallbackContext):
@@ -16,22 +20,22 @@ def start(update: Update, context: CallbackContext):
 
 
 def analyse(update: Update, context: CallbackContext):
-    user_global_toxicity_data = UserToxicityData()
     user_chat_toxicity_data = UserToxicityData()
+    user_global_bot_toxicity_data = UserToxicityData()
 
-    if "toxicity" in context.user_data:
-        user_global_toxicity_data = context.user_data["toxicity"]
     if __get_user_key(update.message.from_user) in context.chat_data:
         user_chat_toxicity_data = context.chat_data[__get_user_key(update.message.from_user)]
-
-    user_global_toxicity_data.messages_count += 1
-    user_global_toxicity_data.total_toxicity += SentimentAnalyzer.get_toxicity(update.message.text)
+    if __get_user_key(update.message.from_user) in context.bot_data:
+        user_global_bot_toxicity_data = context.chat_data[__get_user_key(update.message.from_user)]
 
     user_chat_toxicity_data.messages_count += 1
     user_chat_toxicity_data.total_toxicity += SentimentAnalyzer.get_toxicity(update.message.text)
 
-    context.user_data["toxicity"] = user_global_toxicity_data
+    user_global_bot_toxicity_data.messages_count += 1
+    user_global_bot_toxicity_data.total_toxicity += SentimentAnalyzer.get_toxicity(update.message.text)
+
     context.chat_data[__get_user_key(update.message.from_user)] = user_chat_toxicity_data
+    context.bot_data[__get_user_key(update.message.from_user)] = user_global_bot_toxicity_data
 
 
 def get_top_toxics(update: Update, context: CallbackContext):
@@ -44,11 +48,11 @@ def get_top_toxics(update: Update, context: CallbackContext):
 
 def my_toxicity(update: Update, context: CallbackContext):
     user_key = __get_user_key(update.message.from_user)
-    if 'toxicity' not in context.user_data:
+    if user_key not in context.bot_data:
         context.bot.send_message(chat_id=update.effective_chat.id,
                                  text='Общий коэффициент токсичности {0} еще не рассчитан'.format(user_key))
         return
-    toxicity_data = context.user_data['toxicity']
+    toxicity_data = context.bot_data[user_key]
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text='Общий коэффициент токсичности {0} равен {1}'.format(user_key,
                                                                                        toxicity_data.get_toxic_coefficient()))
@@ -62,8 +66,27 @@ def my_toxicity_here(update: Update, context: CallbackContext):
         return
     toxicity_data = context.chat_data[user_key]
     context.bot.send_message(chat_id=update.effective_chat.id,
-                             text='Коэффициент токсичности {0} в этом чате равен {1}'.format(user_key,
-                                                                                             toxicity_data.get_toxic_coefficient()))
+                             text='Коэффициент токсичности {0} в этом чате равен {1}'
+                             .format(user_key, toxicity_data.get_toxic_coefficient()))
+
+
+def lookToxicity(update: Update, context: CallbackContext):
+    """Handle the inline query."""
+    query = update.inline_query.query
+    if query not in context.bot_data:
+        update.inline_query.answer([])
+        return
+
+    toxicity_data: UserToxicityData = context.bot_data[query]
+    results = [
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title='Токсичность {0} равна {1}'.format(query, toxicity_data.get_toxic_coefficient()),
+            input_message_content=InputTextMessageContent(
+                'Токсичность {0} равна {1}, учтено {2} сообщений'.format(query, toxicity_data.get_toxic_coefficient(),
+                                                                         toxicity_data.messages_count)))]
+
+    update.inline_query.answer(results)
 
 
 def __get_user_key(user: User) -> str:
@@ -88,6 +111,8 @@ def main():
     dispatcher.add_handler(my_toxicity_here_handler)
     analyse_handler = MessageHandler(Filters.text, analyse)
     dispatcher.add_handler(analyse_handler)
+    dispatcher.add_handler(InlineQueryHandler(lookToxicity))
+
     updater.start_polling()
     updater.idle()
 
